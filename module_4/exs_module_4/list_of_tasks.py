9b4460d17c60e0237537a69a9178497c140641c8
GuilhermeEllerkmann <guilherme.ellerkmannsilva@gmail.com>
2023-12-06 03:11:48 -0300

    First Commit via CLI test
    I was not able to study too much today because I did an exercise that took a lot of time to do. Why did it take so long? I tried to do it by myself, I think thats why, but it was good because I did it; I managed to complete it without any help, and it was very gratifying to do so. I tried to learn about commits on Github via CLI; actually, this is a commit via CLI. I did around 4 hours of study, and this week I will finish the fourth module. I am really excited for the next one

---
+++ module_4/exs_module_4/list_of_tasks.py
@@ -0,0 +1,49 @@
+import os
+
+list_of_tasks = []
+list_of_erased_tasks = []
+
+def listar(list):
+    if not list:
+        return print('Nothing to list\n')
+    
+    for i in list:
+        print(f'{i}\n')
+
+def undo(lista1, lista2):
+    if not lista1:
+        return print('Nothing to undo\n')
+    
+    last_item = lista1.pop()
+    lista2.append(last_item)
+    
+
+def redo(lista1, lista2):
+    if not lista2:
+        return print('Nothing to redo\n')
+    
+    newest_redo_item = lista2.pop()
+    lista1.append(newest_redo_item)
+
+def add(task, list_of_tasks):
+    task = task.strip()
+    if not task:
+        return print('You did not type any tasks')
+    
+    list_of_tasks.append(task)
+        
+while True:
+
+    print('Commands: list, undo, redo\n')
+    user_choice = str(input('Type an task or an command:'))
+
+    if user_choice.lower() == 'clear':
+        os.system('clear')
+    elif user_choice.lower() == 'list':
+        listar(list_of_tasks)
+    elif user_choice.lower() == 'undo':
+      undo(list_of_tasks, list_of_erased_tasks)
+    elif user_choice.lower() == 'redo':
+       redo(list_of_tasks, list_of_erased_tasks)
+    else:
+        add(user_choice, list_of_tasks)
