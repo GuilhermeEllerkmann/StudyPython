f67ff843722380ab63d29c10cc62d4194fa93df4
GuilhermeEllerkmann <guilherme.ellerkmannsilva@gmail.com>
2023-12-15 04:46:09 -0300

    Late-night study session from December 14th to December 15th.
    Today, I didn't attend many classes because I was participating in challenges on the CodeWars site. It is very enjoyable, and it helps with logical thinking. I completed about three challenges and elevated my level on the site. After that, I attended some classes, but I wasn't understanding well what was being taught. Therefore, I created some exercises with GPT, and it provided me with a very difficult one. I got stuck on this exercise, and I still am, but it's too late now; I need to sleep. I will try to tackle it later. I completed only one part of the exercise, and it took me around 30 minutes just one association with two classes. I need more time to work on it, and I really don't have the time now.

---
+++ module_5/EX_by_chatgpt/composition_class_ex.py
@@ -0,0 +1,61 @@
+# Exercício sobre Agregação em Python
+
+# Imagine um sistema de gerenciamento de biblioteca. Crie três classes: Livro, Autor, e Biblioteca.
+
+#     Livro: Representa um livro com atributos como título, ISBN, e ano de publicação.
+
+#     Autor: Representa um autor com atributos como nome e nacionalidade. Um autor pode ter escrito vários livros.
+
+#     Biblioteca: Representa uma biblioteca que contém uma coleção de livros. Cada livro na biblioteca está associado a um autor.
+
+# Tarefas:
+
+#     Crie instâncias de autores e livros, associando livros a autores.
+#     Adicione livros à biblioteca, garantindo a agregação entre a biblioteca e os livros.
+#     Exiba a lista de livros na biblioteca com detalhes, incluindo o nome do autor de cada livro.
+
+# Este exercício ajudará a entender como as classes podem estar associadas de forma agregada, onde uma classe contém uma coleção de instâncias de outra classe, mas ambas podem existir independentemente.
+
+
+class Book():
+
+    def __init__(self, title, isbn, data_published):
+        self.title = title
+        self.isbn = isbn
+        self.data_published = data_published
+        self._autor = None
+
+    @property
+    def autor(self):
+        return self._autor
+    
+    @autor.setter
+    def set_autor(self, autor):
+        self._autor = autor
+
+    def escrever(self):
+        return f'{self._autor} eh o escritor'
+
+
+class Author():
+
+    def __init__(self, name, born):
+        self.name = name
+        self.born = born
+
+    def escrever(self):
+        return f'{self.name} eh o escritor'
+
+
+class Library():
+
+    def __init__(self, books, author):
+        self.books = books
+        self.author = author
+
+
+autor = Author('Gui', 'Lins')
+livro = Book('a rosa de saron', '123', '22/07')
+livro.set_autor = autor
+
+print(livro.autor.escrever())
